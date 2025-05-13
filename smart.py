e82468705076a28ea480b1612874d92e66e8f338
Yogesh Raje <yhraje@gmail.com>
2025-05-13 15:00:45 +0530

    Add files via upload

---
+++ smart.py
@@ -0,0 +1,34 @@
+import requests
+ 
+API_KEY = "LTxz3YZ5y_6Hv1nyPvrgUqzawDSlEjUgNkcxQJJuRbwE"
+ 
+token_response = requests.post(
+    'https://iam.cloud.ibm.com/identity/token', 
+    data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'}
+)
+mltoken = token_response.json()["access_token"]
+ 
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+ 
+content = "Give me 2 days veg diet plan."
+role = "user"
+ 
+payload_scoring = {
+    "messages": [
+        {"content": content, "role": role}
+    ]
+}
+ 
+response_scoring = requests.post(
+    'https://eu-de.ml.cloud.ibm.com/ml/v4/deployments/224b60a0-1a71-44ff-8a09-c6af388d9182/ai_service?version=2021-05-01',
+    json=payload_scoring,
+    headers={'Authorization': 'Bearer ' + mltoken}
+)
+ 
+print("Scoring response")
+try:
+    print(response_scoring.json())
+except ValueError:
+    print(response_scoring.text)
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
